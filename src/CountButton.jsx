10a98af2e462a53a5257ceb6d220e53b98b8b2b7
Jose Antonio Cabello <joss.antoniocabello@gmail.com>
2024-12-19 19:27:47 -0600

    first commit

---
+++ src/CountButton.jsx
@@ -0,0 +1,27 @@
+import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
+
+export default function CountButton({ type, setCount, locked = false }) {
+  const handleClick = (event) => {
+    setCount((prev) => {
+      if (type === "minus") {
+        const newCount = prev - 1;
+        if (newCount < 0) {
+          return 0;
+        }
+        return newCount;
+      } else {
+        return prev + 1;
+      }
+    });
+    event.currentTarget.blur();
+  };
+  return (
+    <button disabled={locked} onClick={handleClick} className="count-btn">
+      {type === "minus" ? (
+        <MinusIcon className="count-btn-icon" />
+      ) : (
+        <PlusIcon className="count-btn-icon" />
+      )}
+    </button>
+  );
+}
